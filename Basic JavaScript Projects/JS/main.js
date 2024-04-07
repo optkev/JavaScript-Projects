62c4625f016d868ea624ecef7a81bf1cf7e4814c
optkev <github.impulsive012@passinbox.com>
2024-04-06 23:31:25 -0700

    Update

---
+++ Basic JavaScript Projects/JS/main.js
@@ -0,0 +1,11 @@
+var A = "This is the beginning of the \string\"";
+document.write(A);
+window.alert(A)
+
+var B = "and this is the end of the string,"  
++ " this is a concatenate.";
+
+
+document.write(B)
+
+document.write(5 + 5) //This is a expression and a statement.
