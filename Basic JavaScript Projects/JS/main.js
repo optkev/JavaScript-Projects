4db0f508c34d6b0079e575dc0ad69b7f50a73a5f
optkev <github.impulsive012@passinbox.com>
2024-04-08 13:38:47 -0700

    update

--- Basic JavaScript Projects/JS/main.js
+++ Basic JavaScript Projects/JS/main.js
@@ -1,11 +1,15 @@
 var A = "This is the beginning of the \string\"";
 document.write(A);
-window.alert(A)
+window.alert(A);
 
 var B = "and this is the end of the string,"  
 + " this is a concatenate.";
 
 
-document.write(B)
+document.write(B);
 
-document.write(5 + 5) //This is a expression and a statement.
+document.write(5 + 5); //This is a expression and a statement.
+
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+var Sent3 = Sent1 + Sent2;
---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,24 @@
+function addition_Function() {
+    var addition = 4 + 6;
+    document.getElementById("Math").innerHTML = "4 + 6 = " + addition;
+}
+
+function subtraction_Function() {
+    var Subtraction = 5 - 2;
+    document.getElementById("Subtraction").innerHTML = "5 - 2 = " + Subtraction;
+}
+
+function multiplication() {
+    var simple_Math = 6 * 8;
+    document.getElementById("Multiplication").innerHTML = "6 x 8 = " + simple_Math;
+}
+
+function division() {
+    var simple_Math = 48 / 6;
+    document.getElementById("Division").innerHTML = "48 / 6 = " + simple_Math;
+}
+
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 -5;
+    document.getElementById("Multiple_operations").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
+}
