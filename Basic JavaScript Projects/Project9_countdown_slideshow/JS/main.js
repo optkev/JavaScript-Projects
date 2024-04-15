1dcf9f7f97412aa39a2f63db907844490db7adf4
optkev <github.impulsive012@passinbox.com>
2024-04-15 12:37:53 -0700

    Update

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -1,7 +1,23 @@
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlide(n) {
+function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
@@ -11,16 +27,15 @@ function currentSlide(n) {
 
 function showSlides(n) {
     let i;
-    let slides =
-    document.getElementByClassName("mySlides");
-    let dots = document.getElementByClassName("dot");
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.relace("active", "");
+        dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
