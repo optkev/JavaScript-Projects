b1aa7dd5a53ce3f3ec6e2a8a109ccfb7159c5d50
optkev <github.impulsive012@passinbox.com>
2024-04-20 12:09:11 -0700

    Update

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,39 @@
+function Length() {
+    let text = "vel felis sit amet porta";
+    let length = text.length;
+
+    document.getElementById("len").innerHTML = length;
+}
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content
+}
+
+function array_Function() {
+    var Dog_Picture = [];
+    Dog_Picture[0] = "sleeping";
+    Dog_Picture[1] = "playing";
+    Dog_Picture[2] = "eating";
+    Dog_Picture[3] = "barking";
+    document.getElementById("Array").innerHTML = "In this picture, the dog is " + Dog_Picture[0] + ".";
+}
+
+function constant_function() {
+    const Musical_Instrument = {type:"guitar", brand:"Fender", color:"red"};
+    Musical_Instrument.color = "purple";
+    Musical_Instrument.price = "200";
+    Musical_Instrument.strings = "Elixir Nanoweb";
+    document.getElementById("Constant").innerHTML = "The best acoustic " + Musical_Instrument.type + " strings is " + Musical_Instrument.strings;
+}
+
+function return_pi() {
+    return Math.PI;
+}
+
+document.getElementById("return_it").innerHTML = return_pi();
